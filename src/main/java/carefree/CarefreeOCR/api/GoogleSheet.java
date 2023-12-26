950750565a851fc35810cb7d10312c8cfd02fee6
CarefreeLife <csm4903@naver.com>
2023-12-26 11:33:01 +0900

    update

--- src/main/java/carefree/CarefreeOCR/api/GoogleSheet.java
+++ src/main/java/carefree/CarefreeOCR/api/GoogleSheet.java
@@ -42,7 +42,7 @@ public class GoogleSheet {
 //        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
 //                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
         HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(
-                GoogleCredentials.fromStream(new FileInputStream("gangsan21-ocr-6e01aae86a2f.json"))
+                GoogleCredentials.fromStream(new FileInputStream("/home/ec2-user/app/gangsan21-ocr-6e01aae86a2f.json"))
                         .createScoped(Collections.singletonList("https://www.googleapis.com/auth/spreadsheets"))
         );
 
