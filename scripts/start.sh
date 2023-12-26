efb771e32396d8f7b786b83520172c04283ab72b
CarefreeLife <csm4903@naver.com>
2023-12-26 10:45:44 +0900

    Refactor Scripts/Start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,16 @@ ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+cho "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
+        echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 "$CURRENT_PID"
+        sleep 5
+fi
 
 chmod +x $JAR_FILE
 
